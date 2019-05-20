7f23629218e5ba9ef5e50dcb04e41c9a057d0b65
A Hashim <ahashim983@yahoo.com>
2019-05-20 10:34:57 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(games){
+  let findResult = games.find(function(e){
+   return e.result == "W"
+})
+  if (findResult != undefined){
+    return findResult.year
+  } else {
+    return undefined
+  }
+}
