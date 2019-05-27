ec8a5408c49ecd2c01756fd4ef2666266f1c72fa
endyranaudo <endyranaudo@gmail.com>
2019-05-27 16:00:22 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = results => {
+  let result = results.find( match => match.result === "W" )
+  return result ? result.year : undefined;
+}
